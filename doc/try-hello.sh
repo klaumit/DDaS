6c97bc4c1678a7bafd80f5cac50e879d202e90cb
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-17 23:21:42 +0100

    Compiling to com

--- doc/try-hello.sh
+++ doc/try-hello.sh
@@ -4,3 +4,7 @@ curl -X POST "http://localhost:5050/api/code/compile/asm" \
      -F "file=@./hello.c" \
      -o hello.s
 
+curl -X POST "http://localhost:5050/api/code/compile/com" \
+     -F "file=@./hello.c" \
+     -o hello.com
+
