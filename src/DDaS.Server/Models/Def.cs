efc85ea09dfcc28c31322bcd4fe47e1e6e668304
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-19 00:58:35 +0100

    Adding select

---
+++ src/DDaS.Server/Models/Def.cs
@@ -0,0 +1,10 @@
+using DDaS.Core.API;
+using DDaS.Core.Impl;
+
+namespace DDaS.Server.Models
+{
+    public static class Def
+    {
+        public static readonly ICompilers Compilers = new Compilers();
+    }
+}
