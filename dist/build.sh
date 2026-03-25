7bae7eaebbcf2b0f6cc97c97ecbf7ac726b74e11
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-25 23:44:40 +0100

    Merging scripts

--- dist/build.sh
+++ dist/build.sh
@@ -4,4 +4,6 @@ dotnet publish -c Release -r linux-x64 --sc ../src/DDaS.Server -o output
 
 docker build -f DDaS.Dockerfile -t ddas_srv .
 
+docker run -p 5050:8080 -it --rm ddas_srv
+
 
--- dist/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker run -p 5050:8080 -it --rm ddas_srv
-
-
