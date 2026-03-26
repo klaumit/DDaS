4673c81346a7cc4f88605e4ef4e340c3e3abd93b
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-26 23:30:03 +0100

    Adding tests for core

---
+++ src/DDaS.Tests/AssemblerTest.cs
@@ -0,0 +1,18 @@
+﻿using DDaS.Core.Assemblers.API;
+using DDaS.Core.Compilers.API;
+using DDaS.Core.Disassemblers.API;
+using Xunit;
+
+namespace DDaS.Tests
+{
+    public class AssemblerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            IAssemblers a;
+            IDisassemblers b;
+            ICompilers c;
+        }
+    }
+}
---
+++ src/DDaS.Tests/CompilerTest.cs
@@ -0,0 +1,18 @@
+using DDaS.Core.Assemblers.API;
+using DDaS.Core.Compilers.API;
+using DDaS.Core.Disassemblers.API;
+using Xunit;
+
+namespace DDaS.Tests
+{
+    public class CompilerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            IAssemblers a;
+            IDisassemblers b;
+            ICompilers c;
+        }
+    }
+}
