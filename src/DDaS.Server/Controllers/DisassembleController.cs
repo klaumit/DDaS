70d42fc50053b60202d5e7757b6e77d49e868e11
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-03-26 22:19:28 +0100

    Separating controllers

--- src/DDaS.Server/Controllers/DisassembleController.cs
+++ src/DDaS.Server/Controllers/DisassembleController.cs
@@ -1,42 +1,42 @@
 using System.Threading.Tasks;
-using DDaS.Core.Assemblers.API;
+using DDaS.Core.Disassemblers.API;
 using DDaS.Core.Tools;
 using DDaS.Server.Tools;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using static DDaS.Server.Tools.WebTool;
-using AS = DDaS.Core.Assemblers.API.IAssemblers;
+using AS = DDaS.Core.Disassemblers.API.IDisassemblers;
 
 namespace DDaS.Server.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AssembleController : ControllerBase
+    public class DisassembleController : ControllerBase
     {
         private static readonly string TmpDir = FileTool.CreateOrGetDir("tmp")!;
 
         private readonly AS _api;
 
-        public AssembleController(AS api)
+        public DisassembleController(AS api)
         {
             _api = api;
         }
 
-        [HttpGet("ids", Name = "AllAssembleIds")]
+        [HttpGet("ids", Name = "AllDisassembleIds")]
         public OkObjectResult Get()
         {
-            return Ok(_api.ListAssemblerInfo());
+            return Ok(_api.ListDisassemblerInfo());
         }
 
-        [HttpPost("{id}", Name = "Assemble")]
-        public async Task<IActionResult> Assemble(AssembleId id, IFormFile? file)
+        [HttpPost("{id}", Name = "Disassemble")]
+        public async Task<IActionResult> Assemble(DisassembleId id, IFormFile? file)
         {
             if (file.IsEmpty() is not { } f)
                 return BadRequest("No file provided!");
 
             using var inputFile = await Save(TmpDir, f);
-            var asm = _api.GetAssembler(id);
-            var exec = await asm.Assemble(inputFile);
+            var asm = _api.GetDisassembler(id);
+            var exec = await asm.Disassemble(inputFile);
             HttpContext.SetHeaders(exec);
             using var outputFile = exec.File;
             return ToFile(this, outputFile);
