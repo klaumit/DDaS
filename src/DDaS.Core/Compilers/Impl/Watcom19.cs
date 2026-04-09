93cd6105db1e51b7ad634ff9aeefe1b9ec9b1592
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-04-10 01:19:44 +0200

    Adding steps

--- src/DDaS.Core/Compilers/Impl/Watcom19.cs
+++ src/DDaS.Core/Compilers/Impl/Watcom19.cs
@@ -21,6 +21,22 @@ namespace DDaS.Core.Compilers.Impl
         private const string B = @"D:\w19";
         private const string E = "WCC";
 
+/*
+
+#!/bin/sh
+
+nasm -f obj s.asm -o s.obj
+
++++++++
+
+wcc -1 -os -zls -zl -ms -s -d2 hello.c -fo=main.obj
+
++++++++
+
+wlink @t.lnk option nodefaultlibs option start=_s_ option statics system t file {main.obj s.obj} name x.com
+
+*/
+
         public async Task<Executed> CompileToAsm(IFile input)
             => await Compile(_log, input, [B, E, "-1", "-S"], AsmExt, RunExe);
 
